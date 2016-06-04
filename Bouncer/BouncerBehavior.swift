cdc73afca75309358a37bb142e1d9c2937eecabd
romek <romek@toywar.ru>
2016-06-04 14:00:21 +0300

    initial commit

---
+++ Bouncer/BouncerBehavior.swift
@@ -0,0 +1,55 @@
+//
+//  BouncerBehavior.swift
+//  Bouncer
+//
+//  Created by Roman Ekimov on 03.06.16.
+//  Copyright © 2016 romek. All rights reserved.
+//
+
+import UIKit
+
+class BouncerBehavior: UIDynamicBehavior {
+    
+    let gravity = UIGravityBehavior()
+    
+    lazy var collider: UICollisionBehavior = {
+        let lazilyCreatedCollider = UICollisionBehavior()
+        lazilyCreatedCollider.translatesReferenceBoundsIntoBoundary = true
+        return lazilyCreatedCollider
+    }()
+    
+    lazy var blockBehavior: UIDynamicItemBehavior = {
+        let lazilyCreatedBlockBehavior = UIDynamicItemBehavior()
+        lazilyCreatedBlockBehavior.allowsRotation = true
+        lazilyCreatedBlockBehavior.elasticity = 0.85
+        lazilyCreatedBlockBehavior.friction = 0
+        lazilyCreatedBlockBehavior.resistance = 0
+        return lazilyCreatedBlockBehavior
+    }()
+    
+    override init() {
+        super.init()
+        addChildBehavior(gravity)
+        addChildBehavior(collider)
+        addChildBehavior(blockBehavior)
+    }
+    
+    func addBlock(block: UIView) {
+        dynamicAnimator?.referenceView?.addSubview(block)
+        gravity.addItem(block)
+        collider.addItem(block)
+        blockBehavior.addItem(block)
+    }
+    
+    func removeBlock(block: UIView) {
+        gravity.removeItem(block)
+        collider.removeItem(block)
+        blockBehavior.removeItem(block)
+        block.removeFromSuperview()
+    }
+    
+    func addBarrier(path: UIBezierPath, named name:String) {
+        collider.removeBoundaryWithIdentifier(name)
+        collider.addBoundaryWithIdentifier(name, forPath: path)
+    }
+}
